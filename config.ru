45e37bdb4788034722058369bac9fb1213dc88c7
Frédérick Ros <frederick.ros@gmail.com>
2010-11-22 01:55:25 +0100

    Poussr works git status Needs more polishing though

--- config.ru
+++ config.ru
@@ -1,33 +1,11 @@
 require 'rubygems'      # <-- Added this require
 require 'em-websocket'
-#require 'sinatra/base'
 require 'poussr/rest_api'
+require 'poussr/ws_handler'
 require 'thin'
 
-EventMachine.run do     # <-- Changed EM to EventMachine
-  # class App < Sinatra::Base
-  #     get '/' do
-  #         return "foo"
-  #     end
-  # end
+EventMachine.run do
+  Poussr::WSHandler.start('0.0.0.0', 8080)
 
-  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws| # <-- Added |ws|
-      # Websocket code here
-      ws.onopen {
-          ws.send "connected!!!!"
-      }
-
-      ws.onmessage { |msg|
-          puts "got message #{msg}"
-      }
-
-      ws.onclose   {
-          ws.send "WebSocket closed"
-      }
-
-  end
-
-  # You could also use Rainbows! instead of Thin.
-  # Any EM based Rack handler should do.
-  Poussr::RESTApi.run!({:port => 3000})    # <-- Changed this line from Thin.start to App.run!
+  Poussr::RESTApi.run!({:port => 3000})
 end
---
+++ lib/poussr/ws_handler.rb
@@ -0,0 +1,56 @@
+require 'eventmachine'
+require 'poussr/channel'
+
+module Poussr
+  class WSHandler
+
+    def self.subscribe(ws, channel_name)
+      puts "FRED: subscription to channel: #{channel_name} received"
+      channel = Channel.find_or_create( channel_name)
+      sock_id = channel.em_channel.subscribe {|msg| ws.send msg }
+    end
+
+    def self.unsubscribe(ws, channel_name)
+      channel = Channel.find_or_create( channel_name)
+      sock_id = channel.em_channel.unsubscribe {|msg| ws.send msg }
+    end
+
+    def self.start(host, port)
+      # FIXME: hardcoded host and port
+      EventMachine::WebSocket.start(:host => host, :port => port, :debug => true) do |ws|
+        
+        ws.onopen {
+          ws.send "connected!!!!"
+        }
+
+        ws.onmessage { |msg|
+          # Let's check the message
+          # FIXME: Ensure we have JSON ...
+          begin
+            msg_event = JSON.parse(msg)
+          rescue JSON::ParserError => e
+            STDERR.puts "Error: message is probably not JSON #{e}"
+            msg_event = {'event' => ''} 
+          end
+          
+          case msg_event['event']
+          when 'poussr:subscribe'
+            # Let's create the channel if it's not existing
+            # and associate this ws to the right EM::Channel
+            WSHandler.subscribe(ws,msg_event['channel'])
+          when 'poussr::unsusbscribe'
+            # Let's unbind from the channel and associated EM::Channel
+            #            WSHandler.unsubscribe(ws,msg_event['channel'])
+          else
+            puts "got message #{msg}"
+          end
+        }
+
+        ws.onclose   {
+          ws.send "WebSocket closed"
+        }
+        
+      end
+    end
+  end
+end
