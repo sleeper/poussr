bd4f86e936294efb8bb9e946f19c3f74f30b21fd
Frederick Ros <frederick.ros@gmail.com>
2011-04-05 22:25:57 +0200

    Updated config

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,9 @@ require 'poussr/environment'
 # Set host for Poussr
 Poussr::Environment.configure do |cfg|
   cfg.ws_host "0.0.0.0"
-  cfg.ws_port 8080
+  cfg.ws_port 3031
   cfg.ws_debug true
 
-  cfg.port 3000  # REST API server port
+  cfg.port 3030  # REST API server port
 end
 
